c7042cddfc66bac780203a42143f6821551c5099
Helena <helena.petruz@gmail.com>
2026-05-25 06:50:46 -0300

    Create ex17.c

---
+++ ex17.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void romano(int, char[]);
+
+
+int main(){
+    int n;
+    char romano[];
+    printf("N: ");
+    scanf("%d", &n);
+    romano(n, romano);
+    printf("Em algarismos romanos: %d", romano);
+}
+
+void romano(int n, char romano){
+    
+}
