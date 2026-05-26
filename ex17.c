a238b70ff818e1a3e05aee945b1d684f4b1ac537
Helena <helena.petruz@gmail.com>
2026-05-25 21:26:39 -0300

    Update ex17.c

--- ex17.c
+++ ex17.c
@@ -5,13 +5,105 @@ void romano(int, char[]);
 
 int main(){
     int n;
-    char romano[];
+    char texto[50];
     printf("N: ");
     scanf("%d", &n);
-    romano(n, romano);
-    printf("Em algarismos romanos: %d", romano);
+    romano(n, texto);
+    printf("Em algarismos romanos: %s", texto);
 }
 
-void romano(int n, char romano){
-    
+void romano(int n, char texto[]){
+    int i = 0;
+
+    while(n >= 1000){
+        texto[i] = 'M';
+        i++;
+        n -= 1000;
+    }
+
+    if(n >= 900){
+        texto[i] = 'C';
+        i++;
+        texto[i] = 'M';
+        i++;
+        n -= 900;
+    }
+
+    while(n >= 500){
+        texto[i] = 'D';
+        i++;
+        n -= 500;
+    }
+
+    if(n >= 400){
+        texto[i] = 'C';
+        i++;
+        texto[i] = 'D';
+        i++;
+        n -= 400;
+    }
+
+    while(n >= 100){
+        texto[i] = 'C';
+        i++;
+        n -= 100;
+    }
+
+    if(n >= 90){
+        texto[i] = 'X';
+        i++;
+        texto[i] = 'C';
+        i++;
+        n -= 90;
+    }
+
+    while(n >= 50){
+        texto[i] = 'L';
+        i++;
+        n -= 50;
+    }
+
+    if(n >= 40){
+        texto[i] = 'X';
+        i++;
+        texto[i] = 'L';
+        i++;
+        n -= 40;
+    }
+
+    while(n >= 10){
+        texto[i] = 'X';
+        i++;
+        n -= 10;
+    }
+
+    if(n >= 9){
+        texto[i] = 'I';
+        i++;
+        texto[i] = 'X';
+        i++;
+        n -= 9;
+    }
+
+    while(n >= 5){
+        texto[i] = 'V';
+        i++;
+        n -= 5;
+    }
+
+    if(n >= 4){
+        texto[i] = 'I';
+        i++;
+        texto[i] = 'V';
+        i++;
+        n -= 4;
+    }
+
+    while(n >= 1){
+        texto[i] = 'I';
+        i++;
+        n -= 1;
+    }
+
+    texto[i] = '\0';
 }
